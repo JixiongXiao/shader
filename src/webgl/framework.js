fc4605be8f61fe3bf47f126218810dd4cd44d0d9
肖继雄 <xiaojixiong@njbestway.com>
2023-09-22 17:49:55 +0800

    webgl学习

--- src/webgl/framework.js
+++ src/webgl/framework.js
@@ -1,33 +1,48 @@
-import Shape from "./basic/shape";
-import GlslLearn from "./basic/glslLearn";
-import Box from "./basic/box";
-
+import THR from "./framework/THR.js";
+import { Vector3 } from "three";
 export default class ThreePlus {
   constructor(dom) {
     this.domElement = document.querySelector(dom);
-    this.vector = { x: 0, y: 0 };
-    this.rotate = 0;
-    this.scale = 0.1;
-    this.count = 0;
-    this.judge = true;
-    this.init();
+    this.state = 0;
+    this.initBox(); // 静态BOX
     this.run();
-  }
-  init() {
-    this.canvas = document.createElement("canvas");
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
-    this.domElement.appendChild(this.canvas);
-
-    // 获取webgl上下文
-    this.gl = this.canvas.getContext("webgl");
-    // this.gl.enable(this.gl.CULL_FACE);
-  }
-  run() {
     this.render();
   }
-
+  initBox() {
+    this.scene = new THR.Scene();
+    this.camera = new THR.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    this.camera.position.set(2, 2, 2);
+    this.camera.lookAt(new Vector3(0, 0, 0));
+    this.renderer = new THR.WebglRenderer(this.domElement);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.domElement.appendChild(this.renderer.domElement);
+    let geometry = new THR.BoxGeometry(1, 1, 1);
+    let material = new THR.MeshBasicMaterial({ color: 0xfff00 });
+    this.cube = new THR.Mesh(geometry, material);
+    this.scene.add(this.cube);
+    this.controls = new THR.OrbitControls(
+      this.camera,
+      this.renderer.domElement
+    );
+  }
+  run() {
+    this.setState(2);
+  }
+  setState(t) {
+    this.state = t;
+  }
   render() {
+    if (this.state === 1) {
+      this.renderer.render(this.scene, this.camera);
+    } else if (this.state === 2) {
+      // this.cube.rotation.y += 0.01;
+      this.renderer.render(this.scene, this.camera);
+    }
     requestAnimationFrame(this.render.bind(this));
   }
 }
---
+++ src/webgl/framework/THR.js
@@ -0,0 +1,16 @@
+import Scene from "./core/Scene.js";
+import PerspectiveCamera from "./core/PersepectiveCamera.js";
+import BoxGeometry from "./core/BoxGeometry.js";
+import Mesh from "./core/Mesh.js";
+import MeshBasicMaterial from "./core/MeshBasicMaterial.js";
+import WebglRenderer from "./core/WebglRender.js";
+import OrbitControls from "./core/OribitControls.js";
+export default {
+  Scene,
+  PerspectiveCamera,
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  WebglRenderer,
+  OrbitControls,
+};
